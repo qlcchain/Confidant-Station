ca9bf6763245422eaaed8f9f4ec90ee8c1ef22fb
willcao <peiqing.cao@qlink.mobi>
2019-08-15 11:02:45 +0800

    changed upgrade shell

--- source/upgrade/preinstall.sh
+++ source/upgrade/preinstall.sh
@@ -1,4 +1,8 @@
 #！/bin/sh
 
 killall process_monitor.sh pnr_server
-
+#check qlc_update.py
+diff /tmp/upgrade/qlc_update.py /root/qlc_update.py >/dev/null
+if  [ "$?" == "0" ];then
+	cp -f /tmp/upgrade/qlc_update.py /root/qlc_update.py
+fi
