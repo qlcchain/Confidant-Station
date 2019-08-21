02acbd0614987b57f39f2199fc91080246ed0593
willcao <peiqing.cao@qlink.mobi>
2019-08-21 16:24:48 +0800

    changed qlc_update script

--- source/gqlcnode_upgrade/preinstall.sh
+++ source/gqlcnode_upgrade/preinstall.sh
@@ -6,3 +6,12 @@ if [ ! -d $gqlcnodepath ]; then
 fi
  
 killall gqlc-confidant
+#check qlc_update.py
+diff /tmp/gqlcnode_upgrade/qlc_update.py /root/qlc_update.py >/dev/null
+if  [ $0 != 0 ];then
+	cp -f /tmp/gqlcnode_upgrade/qlc_update.py /root/qlc_update.py
+fi
+
+#删除原有账本目录
+rm -f nohup.out
+rm -rf /sata/home/gqlcnode/*  
