72af1c9158c9f72fb83888173f03a7df37bbd857
willcao <peiqing.cao@qlink.mobi>
2019-08-22 19:27:43 +0800

    change upgrade scripts

--- source/gqlcnode_upgrade/preinstall.sh
+++ source/gqlcnode_upgrade/preinstall.sh
@@ -7,11 +7,17 @@ fi
  
 killall gqlc-confidant
 #check qlc_update.py
-diff /tmp/gqlcnode_upgrade/qlc_update.py /root/qlc_update.py >/dev/null
-if  [ $0 != 0 ];then
-	cp -f /tmp/gqlcnode_upgrade/qlc_update.py /root/qlc_update.py
+if [ -f /tmp/gqlcnode_upgrade/qlc_update.py ];then
+	diff /tmp/gqlcnode_upgrade/qlc_update.py /root/qlc_update.py >/dev/null
+	if  [ $0 != 0 ];then
+		cp -f /tmp/gqlcnode_upgrade/qlc_update.py /root/qlc_update.py
+	fi
 fi
 
 #删除原有账本目录
-rm -f nohup.out
-rm -rf /sata/home/gqlcnode/*  
+if [ -f /root/nohup.out ];then
+	rm -f nohup.out
+fi
+if [ -d /sata/home/gqlcnode ];then
+	rm -rf /sata/home/gqlcnode/*
+fi
--- source/upgrade/preinstall.sh
+++ source/upgrade/preinstall.sh
@@ -2,7 +2,9 @@
 
 killall process_monitor.sh pnr_server
 #check qlc_update.py
-diff /tmp/upgrade/qlc_update.py /root/qlc_update.py >/dev/null
-if  [ $0 != 0 ];then
-	cp -f /tmp/upgrade/qlc_update.py /root/qlc_update.py
+if [ -f /tmp/upgrade/qlc_update.py ];then
+	diff /tmp/upgrade/qlc_update.py /root/qlc_update.py >/dev/null
+	if  [ $0 != 0 ];then
+		cp -f /tmp/upgrade/qlc_update.py /root/qlc_update.py
+	fi
 fi
