004577de9fe5b75b98a0134c3c82250187605ffe
willcao <peiqing.cao@qlink.mobi>
2019-11-26 17:45:34 +0800

    format version.h file

--- source/inc/version.h
+++ source/inc/version.h
@@ -1,10 +1,10 @@
 #ifndef __VERSION_H
 #define __VERSION_H
 
-#define PNR_SERVER_TOPVERSION 	"%t"
-#define PNR_SERVER_MIDVERSION	"%m"
-#define PNR_SERVER_LOWVERSION 	"%l"
-#define PNR_SERVER_BUILD_TIME	"%T"
+#define PNR_SERVER_TOPVERSION   "%t"
+#define PNR_SERVER_MIDVERSION   "%m"
+#define PNR_SERVER_LOWVERSION   "%l"
+#define PNR_SERVER_BUILD_TIME   "%T"
 #define PNR_SERVER_BUILD_HASH   "%G"
 
 #endif
